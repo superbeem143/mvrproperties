361fba6a6c99268bc99c98b1893c097d50b26f7b
superbeem143 <raju.mandavalli@gmail.com>
2026-07-21 17:11:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 document.querySelector('form button').onclick=()=>{
 alert('Thank you! Replace this with WhatsApp or form submission later.');
 };
+function openPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
